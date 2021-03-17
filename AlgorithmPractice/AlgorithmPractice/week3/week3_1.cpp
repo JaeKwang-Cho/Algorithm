5c067a69dd54b36a4fe57ed62dbd9067a85d5423
Dorasima-98 <cjk1516@gmail.com>
2021-03-17 20:48:13 +0900

    week3 greedy comp 800

--- AlgorithmPractice/AlgorithmPractice/week3/week3_1.cpp
+++ AlgorithmPractice/AlgorithmPractice/week3/week3_1.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
+	__int32 k2, k3, k5, k6;
+
+	cin >> k2 >> k3 >> k5 >> k6;
+
+	__int8 arr32[2] = { 3, 2 };
+	__int8 arr256[3] = { 2,5,6 };
+
+	__int32 sum = 0;
+
+	while (1)
+	{
+		if (k2 > 0 && k5 > 0 && k6 > 0)
+		{
+			sum += 256;
+			k2--;
+			k5--;
+			k6--;
+		}
+		else if(k3 > 0 && k2 > 0)
+		{
+			sum += 32;
+			k3--;
+			k2--;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	cout << sum;
+
 	return 0;
 }
