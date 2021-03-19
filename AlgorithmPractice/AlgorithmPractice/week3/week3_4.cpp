69e6a1d0d8720b89af68306a600064a78da5598a
Dorasima-98 <cjk1516@gmail.com>
2021-03-19 16:51:23 +0900

    fuck i don't know

--- AlgorithmPractice/AlgorithmPractice/week3/week3_4.cpp
+++ AlgorithmPractice/AlgorithmPractice/week3/week3_4.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 int main()
 {
+	int t;
+	cin >> t; 
 
+	while (t--)
+	{
+		int n, m;
+		cin >> n >> m;
+		map<int, int> cnt;
+		while (n--)
+		{
+			int x;
+			cin >> x;
+			cnt[x % m]++;
+		}
+		int ans = 0;
+
+		for (auto &c : cnt)
+		{
+			if (c.first == 0)
+			{
+				cout << "1. " << c.first << "/ "<<c.second<< endl;
+				ans++;
+			}
+			else if(2*c.first == m){
+				cout << "2. " << c.first << "/ " << c.second << endl;;
+				ans++;
+			}
+			else if (2 * c.first < m || cnt.find(m - c.first) == cnt.end())
+			{
+				cout << "3. " << c.first << "/ " << c.second << endl;
+				int x = c.second;
+				int y = cnt[m - c.first];
+				cout << "x is " << x << " y is " << y << endl;
+				ans += 1 + max(0, abs(x - y) - 1);
+			}
+		}
+		cout << ans << endl;
+		// 2, 2, 0, 2, 1, 0
+	}
+	return 0;
 }
