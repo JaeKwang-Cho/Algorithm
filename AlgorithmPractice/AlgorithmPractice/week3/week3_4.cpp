1ceed5a8301352179b8442323f8756beb976a64e
Dorasima-98 <cjk1516@gmail.com>
2021-03-21 16:53:39 +0900

    I don't know what it is

--- AlgorithmPractice/AlgorithmPractice/week3/week3_4.cpp
+++ AlgorithmPractice/AlgorithmPractice/week3/week3_4.cpp
@@ -25,19 +25,15 @@ int main()
 		{
 			if (c.first == 0)
 			{
-				cout << "1. " << c.first << "/ "<<c.second<< endl;
 				ans++;
 			}
 			else if(2*c.first == m){
-				cout << "2. " << c.first << "/ " << c.second << endl;;
 				ans++;
 			}
 			else if (2 * c.first < m || cnt.find(m - c.first) == cnt.end())
 			{
-				cout << "3. " << c.first << "/ " << c.second << endl;
 				int x = c.second;
 				int y = cnt[m - c.first];
-				cout << "x is " << x << " y is " << y << endl;
 				ans += 1 + max(0, abs(x - y) - 1);
 			}
 		}
