c1c4a7fe68531c39bf27cf45ef92c9dd823a2347
Dorasima-98 <cjk1516@gmail.com>
2021-03-26 16:08:57 +0900

    Runtime error O(n^2)

--- AlgorithmPractice/AlgorithmPractice/week4/week4_2.cpp
+++ AlgorithmPractice/AlgorithmPractice/week4/week4_2.cpp
@@ -1,15 +1,91 @@
 #include <iostream>
 
 using namespace std;
+size_t paint_fence(__int32* arr, size_t start, size_t end, __int32 offset);
 
 int main()
 {
-	__int32 y_hp, y_atk, y_def;
-	cin >> y_hp >> y_atk >> y_def;
-	__int32 m_hp, m_atk, m_def;
-	cin >> m_hp >> m_atk >> m_def;
-	__int32 p_hp, p_atk, p_def;
-	cin >> p_hp >> p_atk >> p_def;
+	size_t n;
+	cin >> n;
+	__int32* a = new __int32[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+
+	cout << paint_fence(a, 0, n - 1, 0);
 
 	return 0;
+}
+
+size_t paint_fence(__int32* arr, size_t start, size_t end, __int32 offset)
+{
+	size_t touches = 0;
+
+	size_t num_of_fences = end - start + 1;
+
+	if (num_of_fences == 1)
+	{
+		if (arr[start] == offset)
+		{
+			return 0;
+		}
+		else
+		{
+			return 1;
+		}
+	}
+	if (num_of_fences == 2)
+	{
+		if (arr[start] == arr[end] && arr[start] - offset == 1)
+		{
+			return 1;
+		}
+		else if (arr[start] == offset && arr[end] == offset)
+		{
+			return 0;
+		}
+		else if (arr[start] == offset || arr[end] == offset)
+		{
+			return 1;
+		}
+		else
+		{
+			return 2;
+		}
+	}
+
+	size_t min_index = 0;
+	for (int i = start; i < end; i++)
+	{
+		if (arr[min_index] >= arr[i])
+		{
+			min_index = i;
+		}
+	}
+
+	if (start == min_index)
+	{
+		touches += paint_fence(arr, min_index +1 ,end , arr[min_index]);
+	}
+	else if (end == min_index)
+	{
+		touches += paint_fence(arr, start, min_index-1, arr[min_index]);
+	}
+	else
+	{
+		touches += paint_fence(arr, start, min_index - 1, arr[min_index]);
+		touches += paint_fence(arr, min_index + 1, end, arr[min_index]);
+	}
+
+	touches += (arr[min_index] - offset);
+
+	if (touches >= num_of_fences)
+	{
+		return num_of_fences;
+	}
+	else
+	{
+		return touches;
+	}
 }
